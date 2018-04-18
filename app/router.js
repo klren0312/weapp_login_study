270268a53b89afee8628cfdb996e76ed27279838
klren0312 <kuanglong0312@sina.com>
2018-04-18 17:16:08 +0800

    delete some

--- app/router.js
+++ app/router.js
@@ -5,13 +5,5 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/user/:id', controller.user.index);
-  router.get('/user/:id/:name', controller.user.info);
-  router.get('/search', controller.search.index);
-  router.get('/npm', controller.npm.index);
-  router.get('/getinfo', controller.npm.get);
-  router.get('/postinfo', controller.npm.post);
-  router.get('/getdata', controller.mysql.index);
   router.post('/weapp', controller.weapp.login);
 };
