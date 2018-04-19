0f2c25607eda2e65e55ba17afe6da8f9d2761bdb
klren0312 <kuanglong0312@sina.com>
2018-04-19 18:01:15 +0800

    引入socket,redis,uws

--- config/config.default.js
+++ config/config.default.js
@@ -5,13 +5,16 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1523879140687_7825';
   // add your config here
-  config.middleware = [];
+  config.middleware = ['gzip','uppercase'];
+
+  // 配置csrf为false，方便测试接口
   config.security = {
     csrf: {
       enable: false,
     },
     domainWhiteList: ['*']
   };
+  // 配置mysql
   config.mysql = {
     client: {
       host: '118.89.188.40',
@@ -22,6 +25,31 @@ module.exports = appInfo => {
     },
     app: true,
     agent: false,
-  }
+  };
+  // 配置redis
+  config.redis = {
+    client: {
+      port: 6379,
+      host: '127.0.0.1',
+      password: '',
+      db: 0,
+    }
+  };
+  // 配置socket.io
+  config.io = {
+    init: {
+      wsEngine: 'uws',
+    }, // passed to engine.io
+    namespace: {
+      '/': {
+        connectionMiddleware: ['auth'],
+        packetMiddleware: ['filter'],
+      },
+    },
+    redis: {
+      host:'127.0.0.1',
+      port:6379
+    }
+  };
   return config;
 };
--- config/plugin.js
+++ config/plugin.js
@@ -2,9 +2,24 @@
 
 // had enabled by egg
 // exports.static = true;
+
+// mysql插件
 exports.mysql = {
   enable: true,
   package: 'egg-mysql',
 }
-
- 
+// 验证插件
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+}
+// socket.io插件
+exports.io = {
+  enable: true,
+  package: 'egg-socket.io',
+}
+// redis插件
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+}
